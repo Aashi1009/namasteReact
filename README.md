97df2b3163d222b10cfbf6a17a23fa37775e9900
Aashi1009 <aashibhawsar1009@gmail.com>
2025-02-21 18:15:01 +0530

    new changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 
+
+# Parcel
+-Dev build 
+-Local Server
+-HMR = Hot Module Replacement 
+-File watching alogorithm (written in C++)
+-Caching - faster builds
+-Image optimization
+-Minification of app for production level apps
+-Bundling
+-Compressing of files
+-consistent hashing
+-Code Splitting 
+-Differential Bundling - support older browser
+-Diagnostic
+-Error Handling
+-HTTPS
+-Tree shaking - remove extra code
+-Different dev and prod bundles
+
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
 {/* <div id='parent'>
     <div id="child">
         <h1 id="heading1"><span></span></h1>
@@ -9,10 +12,10 @@
 
 
 const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {class:"child"}, 
+    React.createElement("div", {className:"child"}, 
         React.createElement("h1", {id:"heading1"},React.createElement("span", {}, "H1 inside span" ) 
     ) ),
-    React.createElement("div", {class:"child"}, 
+    React.createElement("div", {className:"child"}, 
         React.createElement("h1", {id:"heading2"},React.createElement("span", {}, "H2 inside span" ) 
     ) )
 ])
@@ -22,5 +25,5 @@ const heading = React.createElement(
     "Hello World from React!"
 );
 console.log(parent); // it will give us an object inside which we have props that contains the attribute
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent); // render function is converting React element(object) into a HTML tag or real DOM nodes.
