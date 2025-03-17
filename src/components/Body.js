5bf9eaea65d0aa5e9243a6f73ca440fb01e81be3
Aashi1009 <aashibhawsar1009@gmail.com>
2025-03-17 18:01:03 +0530

    Namaste react episode 4

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,21 @@
 import { RESTAURANT_LIST } from "../utils/constant";
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 // Body 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(RESTAURANT_LIST)
     return(
       <div className="body-container">
-        <div className="search-container">Search</div>
+        <div className="button-container">
+          <button className="top-rated-restaurant" onClick={() => {
+            const filteredData = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            )
+            setListOfRestaurant(filteredData)
+          }}>Top Rated Restaurant</button>
+        </div>
         <div className="res-container">
-          {RESTAURANT_LIST.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
+          {listOfRestaurant.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
         </div>
   
       </div>
